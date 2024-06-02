b34381ead4ae5d9a5091311c20e13be4974db4ad
Szymon Sowiński <134802662+szymon-sowinski@users.noreply.github.com>
2024-06-02 14:07:12 +0200

    Updated validation for form data

--- app/src/main/java/com/example/invoicegenius/ui/form/FormFragment.kt
+++ app/src/main/java/com/example/invoicegenius/ui/form/FormFragment.kt
@@ -17,6 +17,16 @@ import com.example.invoicegenius.Product
 import com.example.invoicegenius.Seller
 import com.example.invoicegenius.databinding.FragmentFormBinding
 import com.google.gson.Gson
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import android.graphics.Color
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.example.invoicegenius.R
+import java.util.*
 
 open class FormFragment : Fragment() {
 
@@ -93,9 +103,9 @@ open class FormFragment : Fragment() {
 
         _binding = FragmentFormBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        setupFieldListeners()
 
         submitButton.setOnClickListener {
-            Log.d("test-button", binding.validateButton.text.toString())
             if (validateForm()) {
                 var products: Array<Product> = emptyArray()
                 for (i in 0 until 5) {
@@ -137,51 +147,203 @@ open class FormFragment : Fragment() {
 
         return root
     }
+
     private fun validateForm(): Boolean {
         val errors = mutableListOf<String>()
-        if (companyNameSeller.text.isNullOrBlank()) errors.add("Nazwa firmy sprzedawcy jest wymagana.")
-        if (addressSeller.text.isNullOrBlank()) errors.add("Adres sprzedawcy jest wymagany.")
-        if (nipSeller.text.isNullOrBlank()) errors.add("NIP sprzedawcy jest wymagany.")
-        if (phoneNumberSeller.text.isNullOrBlank()) errors.add("Numer telefonu sprzedawcy jest wymagany.")
-        if (bankAccountNumber.text.isNullOrBlank() || bankAccountNumber.text.length != 24) errors.add(
-            "Numer konta sprzedawcy jest niepoprawny."
-        )
+        val currentDate = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        dateFormat.isLenient = false
 
-        if (companyNameBuyer.text.isNullOrBlank()) errors.add("Nazwa firmy kupującego jest wymagana.")
-        if (addressBuyer.text.isNullOrBlank()) errors.add("Adres kupującego jest wymagany.")
-        if (phoneNumberBuyer.text.isNullOrBlank()) errors.add("Numer telefonu kupującego jest wymagany.")
+        val allFieldsEmpty = (
+                companyNameSeller.text.isNullOrBlank() &&
+                        addressSeller.text.isNullOrBlank() &&
+                        nipSeller.text.isNullOrBlank() &&
+                        phoneNumberSeller.text.isNullOrBlank() &&
+                        bankAccountNumber.text.isNullOrBlank() &&
+                        companyNameBuyer.text.isNullOrBlank() &&
+                        addressBuyer.text.isNullOrBlank() &&
+                        phoneNumberBuyer.text.isNullOrBlank() &&
+                        emailBuyer.text.isNullOrBlank() &&
+                        sellDate.text.isNullOrBlank() &&
+                        targetPaymentDate.text.isNullOrBlank() &&
+                        issueDate.text.isNullOrBlank() &&
+                        paymentMethod.text.isNullOrBlank() &&
+                        paymentDate.text.isNullOrBlank()
+                )
+
+        if (allFieldsEmpty) {
+            showErrors(listOf("Proszę wypełnić przynajmniej jedno pole"))
+            return false
+        }
+
+        if (companyNameSeller.text.isNullOrBlank()) {
+            errors.add("Wprowadź nazwę firmy sprzedawcy.")
+            setFieldError(companyNameSeller)
+        }
+        if (addressSeller.text.isNullOrBlank()) {
+            errors.add("Wprowadź adres sprzedawcy.")
+            setFieldError(addressSeller)
+        }
+        if (nipSeller.text.isNullOrBlank()) {
+            errors.add("Wprowadź NIP sprzedawcy.")
+            setFieldError(nipSeller)
+        }
+        if (phoneNumberSeller.text.isNullOrBlank()) {
+            errors.add("Wprowadź numer telefonu sprzedawcy.")
+            setFieldError(phoneNumberSeller)
+        }
+        if (bankAccountNumber.text.isNullOrBlank() || bankAccountNumber.text.length != 24) {
+            errors.add("Wprowadź poprawny numer konta sprzedawcy (24 cyfry).")
+            setFieldError(bankAccountNumber)
+        }
+
+        if (companyNameBuyer.text.isNullOrBlank()) {
+            errors.add("Wprowadź nazwę firmy kupującego.")
+            setFieldError(companyNameBuyer)
+        }
+        if (addressBuyer.text.isNullOrBlank()) {
+            errors.add("Wprowadź adres kupującego.")
+            setFieldError(addressBuyer)
+        }
+        if (phoneNumberBuyer.text.isNullOrBlank()) {
+            errors.add("Wprowadź numer telefonu kupującego.")
+            setFieldError(phoneNumberBuyer)
+        }
         if (emailBuyer.text.isNullOrBlank() || !android.util.Patterns.EMAIL_ADDRESS.matcher(
                 emailBuyer.text
             ).matches()
         ) {
-            errors.add("Adres e-mail kupującego jest niepoprawny.")
+            errors.add("Wprowadź poprawny adres e-mail kupującego.")
+            setFieldError(emailBuyer)
         }
 
-        if (sellDate.text.isNullOrBlank()) errors.add("Data sprzedaży jest wymagana.")
-        if (issueDate.text.isNullOrBlank()) errors.add("Data wystawienia jest wymagana.")
+        if (sellDate.text.isNullOrBlank()) {
+            errors.add("Wprowadź datę sprzedaży.")
+            setFieldError(sellDate)
+        } else {
+            try {
+                val sellDateFormatted = dateFormat.parse(sellDate.text.toString())
+                if (sellDateFormatted.after(currentDate)) errors.add("Data sprzedaży nie może być w przyszłości.")
+            } catch (e: Exception) {
+                errors.add("Wprowadź datę sprzedaży w formacie DD/MM/RRRR.")
+                setFieldError(sellDate)
+            }
+        }
 
-        if (paymentMethod.text.isNullOrBlank()) errors.add("Metoda płatności jest wymagana.")
-        if (paymentDate.text.isNullOrBlank()) errors.add("Data płatności jest wymagana.")
+        if (issueDate.text.isNullOrBlank()) {
+            errors.add("Wprowadź datę wystawienia faktury.")
+            setFieldError(issueDate)
+        } else {
+            try {
+                val issueDateFormatted = dateFormat.parse(issueDate.text.toString())
+                if (issueDateFormatted.after(currentDate)) errors.add("Data wystawienia nie może być w przyszłości.")
+            } catch (e: Exception) {
+                errors.add("Wprowadź datę wystawienia faktury w formacie DD/MM/RRRR.")
+                setFieldError(issueDate)
+            }
+        }
+
+        if (targetPaymentDate.text.isNullOrBlank()) {
+            errors.add("Wprowadź docelowy termin płatności.")
+            setFieldError(targetPaymentDate)
+        } else {
+            try {
+                val targetPaymentDateFormatted = dateFormat.parse(targetPaymentDate.text.toString())
+            } catch (e: Exception) {
+                errors.add("Wprowadź docelowy termin płatności w formacie DD/MM/RRRR.")
+                setFieldError(targetPaymentDate)
+            }
+        }
+
+        if (paymentMethod.text.isNullOrBlank()) {
+            errors.add("Wprowadź metodę płatności.")
+            setFieldError(paymentMethod)
+        }
+        if (paymentDate.text.isNullOrBlank()) {
+            errors.add("Wprowadź datę płatności.")
+            setFieldError(paymentDate)
+        } else {
+            try {
+                val paymentDateFormatted = dateFormat.parse(paymentDate.text.toString())
+                if (paymentDateFormatted.after(currentDate)) errors.add("Data płatności nie może być w przyszłości.")
+            } catch (e: Exception) {
+                errors.add("Wprowadź datę płatności w formacie DD/MM/RRRR.")
+                setFieldError(paymentDate)
+            }
+        }
 
         if (errors.isNotEmpty()) {
             showErrors(errors)
             return false
         }
-
         return true
     }
 
     private fun showErrors(errors: List<String>) {
         val builder = AlertDialog.Builder(requireActivity())
         builder.setTitle("Błąd walidacji")
-        builder.setMessage(errors.joinToString("\n"))
+
+        val errorMessage = errors.joinToString("\n")
+
+        builder.setMessage(errorMessage)
         builder.setPositiveButton("OK", null)
-        builder.show()
+        val dialog = builder.show()
+
+        val messageTextView = dialog.findViewById<TextView>(android.R.id.message)
+        messageTextView?.setTextColor(Color.RED)
+    }
+
+    private fun setFieldError(field: EditText) {
+        val errorBorder =
+            ContextCompat.getDrawable(requireContext(), R.drawable.edittext_border_error)
+        field.background = errorBorder
+    }
+
+    private fun resetFieldError(field: EditText) {
+        field.background = ContextCompat.getDrawable(requireContext(), R.drawable.edittext_selector)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupFieldListeners() {
+        val fields = listOf(
+            invoiceNumber,
+            companyNameSeller,
+            addressSeller,
+            nipSeller,
+            bankAccountNumber,
+            phoneNumberSeller,
+            companyNameBuyer,
+            addressBuyer,
+            phoneNumberBuyer,
+            emailBuyer,
+            sellDate,
+            issueDate,
+            targetPaymentDate,
+            paymentMethod,
+            paymentDate
+        )
+
+        fields.forEach { field ->
+            field.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    resetFieldError(field)
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+        }
     }
 }
 
@@ -202,3 +364,4 @@ class ProductPositions(
         )
     }
 }
+
